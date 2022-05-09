383daace4e47bfcef6b943bf2e98b235f4ed09dd
Rafael Aguiar <terule@gmail.com>
2022-05-09 10:04:00 -0300

    Feito home, skills e hobbies

---
+++ main.js
@@ -0,0 +1,18 @@
+// abre o menu quando clica no icone hamburger e no x
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+// fecha o menu quando clica em um item
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
